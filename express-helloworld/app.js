132fb75375860df334a85645e064b942fa1aa4a8
nandasatria <nanda.addina@packet-systems.com>
2020-05-12 12:40:10 +0700

    add secret val

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,9 +5,11 @@ app.get('/', function (req, res) {
   res.send('Hello World Version 3!\n');
 });
 
+var username = process.env.username
+var password = process.env.password
 
 app.get('/helo', function (req, res) {
-  res.send('Hello Gaes This is Version 3 !\n');
+  res.send('Hello Gaes This is Version 4 with username '+username+' and password '+password+' !\n');
 });
 
 app.listen(8080, function () {
